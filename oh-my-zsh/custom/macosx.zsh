a9854132d608c33941e0be411ce61077a2ff5860
Adam Stanton <adam.stanton@clio.com>
2021-11-26 09:09:05 -0800

    Add aliases to common working directories

--- oh-my-zsh/custom/macosx.zsh
+++ oh-my-zsh/custom/macosx.zsh
@@ -1,2 +1,5 @@
 export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 export PATH="${PATH}:${HOME}/Library/Python/3.8/bin"
+
+alias cdc="cd ${HOME}/Documents/Projects/clio"
+alias cdp="cd ${HOME}/Documents/Projects/personal"
