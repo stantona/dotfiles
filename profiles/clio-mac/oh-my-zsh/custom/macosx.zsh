f760f398289ed2e9266322cb87bacfdd8a376780
Adam Stanton <adam.stanton@clio.com>
2022-05-13 09:50:43 -0700

    Add script to install kubectl

--- profiles/clio-mac/oh-my-zsh/custom/macosx.zsh
+++ profiles/clio-mac/oh-my-zsh/custom/macosx.zsh
@@ -5,13 +5,6 @@ export DOTBOT_PROFILE_CLIO_MAC=1
 alias cdc="cd ${HOME}/Documents/Projects/clio"
 alias cdp="cd ${HOME}/Documents/Projects/personal"
 
-# Do dev tool related things if it is installed
-if command -v dev &>/dev/null; then
-  eval "$(dev _hook)"
-  # Use the multi architecture peng container
-  alias dev='PENG_IMAGE=peng-multi:latest /usr/local/bin/dev'
-fi
-
 # Enable kubectl auto-completion
 if command -v kubectl &>/dev/null; then
   source <(kubectl completion zsh)
---
+++ profiles/clio-mac/scripts/install_kubectl.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+KUBECTL_VERSION='1.21.0'
+
+if [[ -f /usr/local/bin/kubectl ]]; then
+  sudo rm /usr/local/bin/kubectl
+fi
+
+pushd /tmp
+curl --silent -LO "https://dl.k8s.io/release/v${KUBECTL_VERSION}/bin/darwin/arm64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+sudo chown root: /usr/local/bin/kubectl
+kubectl version --short
+popd
