2ef6a5b01ea15d4bfca616a34ff53a40f12f0279
Adam Stanton <adam.stanton@clio.com>
2022-06-06 08:55:30 -0700

    Add eksctl install

---
+++ profiles/clio-mac/scripts/install_eks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [[ -f /usr/local/bin/eksctl ]]; then
+  sudo rm /usr/local/bin/eksctl
+fi
+
+pushd /tmp
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_Darwin_arm64.tar.gz" | tar xz
+sudo mv /tmp/eksctl /usr/local/bin
+popd
