20ba0fc231a1bac97d85809276100165a5c46874
Adam Stanton <adam.stanton@clio.com>
2023-03-07 12:57:17 -0800

    Add libpq

--- profiles/clio-mac/oh-my-zsh/custom/macosx.zsh
+++ profiles/clio-mac/oh-my-zsh/custom/macosx.zsh
@@ -1,5 +1,7 @@
 export PATH="${PATH}:/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
 export PATH="${PATH}:${HOME}/Library/Python/3.8/bin"
+# Support psql (client postgresql, see: https://stackoverflow.com/questions/44654216/correct-way-to-install-psql-without-full-postgres-on-macos)
+export PATH="${PATH}:/opt/homebrew/opt/libpq/bin"
 export DOTBOT_PROFILE_CLIO_MAC=1
 
 alias cdc="cd ${HOME}/Documents/Projects/clio"
